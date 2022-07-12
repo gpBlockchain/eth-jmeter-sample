c0502cea2b1a6f8ddebce58f99522de748a3a4e7
“Dawn-githap” <13477199767@163.com>
2022-07-12 20:21:45 +0800

    Modify gasprice

---
+++ src/main/java/com/nervos/benckmark/util/DefaultGasProvisder.java
@@ -0,0 +1,19 @@
+package com.nervos.benckmark.util;
+
+import org.web3j.tx.gas.StaticGasProvider;
+
+import java.math.BigInteger;
+
+/**
+ * 1. @description:
+ * 2. @author: Dawn
+ * 3. @time: 2022/7/12
+ */
+public class DefaultGasProvisder extends StaticGasProvider {
+    public static final BigInteger GAS_LIMIT = BigInteger.valueOf(9000000L);
+    public static final BigInteger GAS_PRICE = BigInteger.valueOf(90000000000000L);
+
+    public DefaultGasProvisder() {
+        super(GAS_PRICE, GAS_LIMIT);
+    }
+}
